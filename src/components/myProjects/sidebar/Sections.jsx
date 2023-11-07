333d229a89bb763218002956bbf922a3218e4ce2
viswakirankathi <viswauiux@gmail.com>
2023-11-07 21:37:14 +0530

    okay

--- src/components/myProjects/sidebar/Sections.jsx
+++ src/components/myProjects/sidebar/Sections.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sections({title,icon,menu}) {
   return (
     <div>
-          <div className="my-1 rounded-lg  flex items-center md:justify-start justify-center  hover:bg-slate-200 hover:cursor-pointer   ">
+          <div className={`my-1 rounded-lg  flex items-center   ${menu?"justify-start":" justify-center"}   hover:bg-slate-200 hover:cursor-pointer`}>
              <div className='m-2' title={title}>{icon}</div>
             <h1 className={`font-medium text-sm ${menu?"block":"hidden"}`}>{title} </h1>
           </div>
