cfa84cb468b44286bddc30ab0be2e9e086373368
viswakirankathi <viswauiux@gmail.com>
2023-11-07 21:03:17 +0530

    first commit

--- src/components/myProjects/Projects.jsx
+++ src/components/myProjects/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <> 
     <div className='h-screen flex justify-center items-center'>
-            <Link to={'/sidebar'} className=' p-24 rounded-3xl bg-slate-800 text-white shadow-lg shadow-slate-500 hover:bg-slate-600'>Sidebar</Link>
+            <Link to={'/sidebar'} className=' p-24 rounded-3xl bg-slate-800 text-white shadow-lg shadow-slate-500 hover:bg-slate-600'>My SideBar</Link>
         </div> 
     
     </>
